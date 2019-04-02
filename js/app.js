2b79e0374bc4606ea03fcfb8a8ec2d27b7aa41cc
Velez <jvf_velez@hotmail.com>
2019-04-01 20:13:29 -0600

    Sitio Web con CSS Grid responsive

---
+++ js/app.js
@@ -0,0 +1,27 @@
+var i = 0;
+var j = 0;
+var images = [];
+var efects = [];
+var time = 4000;
+
+// Lista de Imagenes
+images[0] = '/img/001.jpg';
+images[1] = '/img/002.jpg';
+images[2] = '/img/003.jpg';
+images[3] = '/img/004.jpg';
+images[4] = '/img/005.jpg';
+
+// Cambiar Imagen
+function cambiarImg() {
+    document.slide.src = images[i];
+
+    if(i < images.length -1){
+        i++;
+    } else {
+        i = 0;
+    }
+
+    setTimeout("cambiarImg()", time);
+}
+
+window.onload = cambiarImg;
